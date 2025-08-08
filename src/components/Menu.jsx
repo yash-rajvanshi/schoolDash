56385142e2d13bcb809fe0a117b7bb387fd8df0e
yash-rajvanshi <yashrajvanshi@Beast-2.local>
2025-08-08 09:45:12 +0530

    deleted parents

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -40,12 +40,7 @@ const menuItems = [
         href: "/list/students",
         visible: ["admin", "teacher"],
       },
-      {
-        icon: "/parent.png",
-        label: "Parents",
-        href: "/list/parents",
-        visible: ["admin", "teacher"],
-      },
+     
       {
         icon: "/subject.png",
         label: "Subjects",
